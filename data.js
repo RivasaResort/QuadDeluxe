ccd62f72a42c9477a39a56e91a84c60e3db61066
Sanju Kamat <sanjukamat123@gmail.com>
2021-11-18 09:37:34 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,200 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -2.2636999709207384,
+        "pitch": 0.1346181249754963,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6950580543467346,
+          "pitch": 0.03028306234348932,
+          "rotation": 7.853981633974483,
+          "target": "1-2"
+        },
+        {
+          "yaw": -2.025913664573885,
+          "pitch": 0.01965235419456768,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.061644542368448,
+          "pitch": -0.20331558431073127,
+          "title": "QUAD DELUXE ROOM",
+          "text": "<div>Amenities&nbsp;</div><div><br></div><div>- AC</div><div>- TV</div><div>- Sanitized Rooms&nbsp;</div><div>- Daily Housekeeping&nbsp;</div><div>- Free Wifi</div><div>- Swimming pool</div><div>- In house Restaurant &amp; Bar</div><div>- Parking Facility&nbsp;</div><div>- 5 Min walk to the Beach</div><div>- Conference Hall&nbsp;</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.5101596722171085,
+        "pitch": 0.10999119595040696,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4799681666598907,
+          "pitch": 0.11273639541800584,
+          "rotation": 0,
+          "target": "0-1"
+        },
+        {
+          "yaw": -1.5207268152833127,
+          "pitch": 0.03976868128106048,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": -1.0101404749265654,
+          "pitch": 0.06997281731877436,
+          "rotation": 7.853981633974483,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.9966772876089323,
+        "pitch": 0.3135955518944069,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20491265202056042,
+          "pitch": 0.13234459378531405,
+          "rotation": 10.995574287564278,
+          "target": "3-4"
+        },
+        {
+          "yaw": 1.7381954494639835,
+          "pitch": 0.10151215699217353,
+          "rotation": 4.71238898038469,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.9326761018599168,
+          "pitch": 0.0785376827196167,
+          "rotation": 4.71238898038469,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.18112562018708367,
+        "pitch": 0.2407915370806002,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0293155742642854,
+          "pitch": 0.12533215050201107,
+          "rotation": 4.71238898038469,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "QUAD ROOM DELUXE ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
